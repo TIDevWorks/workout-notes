66be2c25ea523b1457ff35822377ced696939d0e
TIDevWorks <ti.devworks@gmail.com>
2024-05-07 18:13:23 +0900

    checkUUIDを関数としてhelperディレクトリに格納

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { useColorScheme } from "@/components/useColorScheme";
+import { checkUUID } from "@/helper/checkUUID";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   DarkTheme,
   DefaultTheme,
@@ -9,11 +9,7 @@ import {
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import * as Updates from "expo-updates";
 import { useEffect } from "react";
-import { Alert } from "react-native";
-import "react-native-get-random-values";
-import { v4 as uuidv4 } from "uuid";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -27,39 +23,6 @@ export const unstable_settings = {
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
-const showAlert = (errorMessage: string) => {
-  Alert.alert("エラー", errorMessage, [
-    {
-      text: "OK",
-      onPress: () => Updates.reloadAsync(),
-    },
-  ]);
-};
-
-const generateAndStoreUUID = async () => {
-  const newUuid = await uuidv4();
-
-  try {
-    await AsyncStorage.setItem("@uuid", newUuid);
-  } catch (e) {
-    console.error(e);
-    showAlert("UUIDの生成と保存に失敗しました。アプリを再起動してください。");
-  }
-};
-
-const checkUUID = async () => {
-  try {
-    const value = await AsyncStorage.getItem("@uuid");
-    console.log(value);
-    if (value === null) {
-      generateAndStoreUUID();
-    }
-  } catch (e) {
-    console.error(e);
-    showAlert("UUIDの確認に失敗しました。アプリを再起動してください。");
-  }
-};
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
---
+++ helper/checkUUID.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Updates from "expo-updates";
+import { Alert } from "react-native";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+
+const showAlert = (errorMessage: string) => {
+  Alert.alert("エラー", errorMessage, [
+    {
+      text: "OK",
+      onPress: () => Updates.reloadAsync(),
+    },
+  ]);
+};
+
+const generateAndStoreUUID = async () => {
+  const newUuid = await uuidv4();
+
+  try {
+    await AsyncStorage.setItem("@uuid", newUuid);
+  } catch (e) {
+    console.error(e);
+    showAlert("UUIDの生成と保存に失敗しました。アプリを再起動してください。");
+  }
+};
+
+export const checkUUID = async () => {
+  try {
+    const value = await AsyncStorage.getItem("@uuid");
+    console.log(value);
+    if (value === null) {
+      generateAndStoreUUID();
+    }
+  } catch (e) {
+    console.error(e);
+    showAlert("UUIDの確認に失敗しました。アプリを再起動してください。");
+  }
+};
